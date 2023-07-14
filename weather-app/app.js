23d1fa36044f94f8b9a50280ed5afe3bb123c67b
Max Kiselyov <maxkiselyov05@gmail.com>
2023-07-14 12:27:52 +0300

    update

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,22 +1,13 @@
 console.log('Starting');
 
-let a = 1;
-
 setTimeout(() => {
-    console.log('2 second loading' + a)
+    console.log('2 second loading')
 }, 2000);
 
-//a++;
 
 setTimeout(() => {
-<<<<<<< HEAD
     console.log('0 second timer');
-=======
-    console.log('0 second timer' + a);
->>>>>>> 9b22c26b9278db94c5c21edd383821a9b05954a1
-    a++;
 }, 0)
-
 
 
 console.log('Stopping');
